f50967efd25c4071bf2d456f676ef4b191e7caae
ollvialee <163212363+ollvialee@users.noreply.github.com>
2025-03-07 15:31:25 -0800

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfbook01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Entity Framework DbContext
+            var dbcontext = new BooksEntities();
+
+            // get authors and ISBNs of each book they co-authored
+            var authorsAndISBNs =
+               from author in dbcontext.Authors
+               from book in author.Titles
+               orderby author.LastName, author.FirstName
+               select new { author.FirstName, author.LastName, title.ISBN};
+
+            outputTextBox.AppendText("Authors and ISBNs:");
+
+            // display authors and ISBNs in tabular format
+            foreach (var element in authorsAndISBNs)
+            {
+                outputTextBox.AppendText($"\r\n\t{element.FirstName,-10} " +
+                   $"{element.LastName,-10} {element.ISBN,-10}");
+            }
+
+            // get authors and titles of each book they co-authored
+            var authorsAndTitles =
+               from book in dbcontext.Titles
+               from author in book.Author
+               orderby author.LastName, author.FirstName, book.Title1
+               select new { author.FirstName, author.LastName, book.Title1};
+
+            outputTextBox.AppendText("\r\n\r\nAuthors and titles:");
+
+            // display authors and titles in tabular format
+            foreach (var element in authorsAndTitles)
+            {
+                outputTextBox.AppendText($"\r\n\t{element.FirstName,-10} " +
+                   $"{element.LastName,-10} {element.Title1}");
+            }
+
+            // get authors and titles of each book 
+            // they co-authored; group by author
+            var titlesByAuthor =
+               from author in dbcontext.Authors
+               orderby author.LastName, author.FirstName
+               select new
+               {
+                   Name = author.FirstName + " " +author.LastName,
+                   Titles =
+                     from book in author.Titles
+                     orderby book.Title1
+                     select book.Title1
+               };
+
+            outputTextBox.AppendText("\r\n\r\nTitles grouped by author:");
+
+            // display titles written by each author, grouped by author
+            foreach (var author in titlesByAuthor)
+            {
+                // display author's name
+                outputTextBox.AppendText($"\r\n\t{author.Name}:");
+
+                // display titles written by that author
+                foreach (var title in author.Titles)
+                {
+                    outputTextBox.AppendText($"\r\n\t\t{title}");
+                }
+            }
+        }
+    }
+}
